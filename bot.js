04c4eda53c3a05013070896be0ca857c23d86c08
Dean Shub <deanshub@gmail.com>
2016-01-30 09:08:51 +0200

    better command syntax

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 //TimeStockBot
 var TelegramBot = require('node-telegram-bot-api');
 var later = require('later');
+var request = require('request');
 
 var token = process.env.BOT_API;
 var options = {
@@ -13,37 +14,89 @@ var options = {
 };
 
 var bot = new TelegramBot(token, options);
+bot.setWebHook('stock.shubapp.com:443/bot'+token,__dirname+'/crt.pem');
 
 var schedules={};
 
 var botDescription="Hi, I\'m TimeStockBot\n"+
     "This is what I can do:\n"+
     "/help to get this message\n"+
-    "/stock <STOCK_SIGN>         - get current stock values\n"+
-    "/graph <STOCK_SIGN>         - get stock 3 day graph\n"+
-    "/sched <STOCK_SIGN> <TIME>  - get stock values at a certain time\n"+
+    "/stock <STOCK_SIGN> - current stock values\n"+
+    "/get <STOCK_SIGN> - same as /stock <STOCK_SIGN>\n"+
+    "/stock <STOCK_SIGN> <TIME> - stock values at a certain time\n"+
+    "/graph <STOCK_SIGN> - stock 3 day graph\n"+
+    "/stock <STOCK_SIGN> cancel - stop stock scheduling\n"+
+    "/unstock <STOCK_SIGN> - same as /stock <STOCK_SIGN> cancel\n"+
     "\nExamples:\n"+
-    "/sched fb every day at 10:00\n"+
-    "For timing text available see http://bunkat.github.io/later/assets/img/Schedule.png";
+    "/stock fb\n"+
+    "/stock fb every day at 10:00\n"+
+    "/unstock fb\n"+
+    "For more information on <TIME>, see http://bunkat.github.io/later/assets/img/Schedule.png";
 
-bot.onText(/\/help/, function (msg, match) {
+bot.onText(/\/help/, helpHandler);
+bot.onText(/\/stock ([^ ]+) (.+)$/, stockAndTimeHandler);
+bot.onText(/\/unstock (.+)$/, cancelStockHandler);
+bot.onText(/\/stock (.+)$/, stockOnlyHandler);
+bot.onText(/\/get ([^ ]+) (.+)$/, stockAndTimeHandler);
+bot.onText(/\/get (.+)$/, stockOnlyHandler);
+bot.onText(/\/graph (.+)$/, graphHandler);
+
+function sendStockInfo(fromId, stockSign){
+  request('http://finance.google.com/finance/info?client=ig&q='+stockSign, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      try{
+        var stockVal = JSON.parse(body.substring(3));
+        bot.sendMessage(fromId, JSON.stringify(stockVal[0], null, 2));
+      }catch(e){}
+    }
+  });
+}
+
+function cancelStockScheduling(fromId, stockSign){
+  if (schedules[fromId] && schedules[fromId][stockSign]){
+      schedules[fromId][stockSign].clear();
+      schedules[fromId][stockSign]=null;
+      bot.sendMessage(fromId, 'OK');
+      return true;
+  }else{
+    bot.sendMessage(fromId, 'I didn\'t find any scheduling on '+ stockSign +'...');
+    return false;
+  }
+}
+
+function graphHandler(msg, match) {
+    var fromId = msg.from.id;
+    var stockSign = match[1];
+
+    bot.sendMessage(fromId, 'http://chart.finance.yahoo.com/z?s='+stockSign+'&t=3d&q=c&l=on&z=l');
+}
+
+function helpHandler(msg, match) {
     var fromId = msg.from.id;
     bot.sendMessage(fromId, botDescription);
+}
+
+function stockOnlyHandler(msg, match) {
+    var fromId = msg.from.id;
+    var stockSign = match[1];
+
+    sendStockInfo(fromId, stockSign);
 });
 
-bot.onText(/\/sched ([^ ]+) (.+)$/, function (msg, match) {
+function cancelStockHandler(msg, match) {
+    var fromId = msg.from.id;
+    var stockSign = match[1];
+
+    cancelStockScheduling(fromId, stockSign);
+});
+
+function stockAndTimeHandler(msg, match) {
   var fromId = msg.from.id;
   var stockSign = match[1];
   var textTime = match[2];
 
   if (textTime && textTime.toUpperCase()==="CANCEL"){
-      if (schedules[fromId] && schedules[fromId][stockSign]){
-          schedules[fromId][stockSign].clear();
-          schedules[fromId][stockSign]=null;
-          bot.sendMessage(fromId, 'OK');
-      }else{
-        bot.sendMessage(fromId, 'I didn\'t find any event on '+ stockSign +'...');
-      }
+      cancelStockScheduling(fromId, stockSign);
   }else{
       var sched = later.parse.text(textTime);
 
@@ -62,26 +115,7 @@ bot.onText(/\/sched ([^ ]+) (.+)$/, function (msg, match) {
 
       bot.sendMessage(fromId, 'OK');
   }
-});
-
-function sendStockInfo(fromId, stockSign){
-    bot.sendMessage(fromId, 'http://finance.google.com/finance/info?client=ig&q='+stockSign);
 }
-
-bot.onText(/\/stock (.+)$/, function (msg, match) {
-    var fromId = msg.from.id;
-    var stockSign = match[1];
-
-    sendStockInfo(fromId, stockSign);
-});
-
-bot.onText(/\/graph (.+)$/, function (msg, match) {
-    var fromId = msg.from.id;
-    var stockSign = match[1];
-
-    bot.sendMessage(fromId, 'http://chart.finance.yahoo.com/z?s='+stockSign+'&t=3d&q=c&l=on&z=l');
-});
-
 // // Any kind of message
 // bot.on('message', function (msg) {
 //   var chatId = msg.chat.id;
